09a049f7473d88d9c9aad65dd8d282b81ce40809
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-08-03 21:24:27 -0700

    changed category routes

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 const { Category } = require("../../models");
 
+router.get("/", (req, res) => {
+    Category.findAll()
+      .then((dbCategoryData) => res.json(dbCategoryData))
+      .catch((err) => {
+        console.log(err);
+        res.statusCode(500).json(err);
+      });
+  });
+  
+
 module.exports = router;
