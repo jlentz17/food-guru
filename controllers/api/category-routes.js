a36277a247413bb59d797df548688189ee6f6746
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-08-04 10:23:05 -0700

    fixed a typo that was keeping the server from running haha

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -1,15 +1,6 @@
 const router = require("express").Router();
 const { Category } = require("../../models");
 const withAuth = require("../../utils/auth")
-
-router.get("/", (req, res) => {
-    Category.findAll()
-      .then((dbCategoryData) => res.json(dbCategoryData))
-      .catch((err) => {
-        console.log(err);
-        res.statusCode(500).json(err);
-      });
-  });
   
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,27 +2,4 @@ const router = require("express").Router();
 const { User, Recipe, Category, Vote } = require("../../models");
 // // Tested rotes to make sure they were connected and hashed passwords correctly
 
-router.get("/", (req, res) => {
-  User.findAll()
-    .then((dbUserData) => res.json(dbUserData))
-    .catch((err) => {
-      console.log(err);
-      res.statusCode(500).json(err);
-    });
-});
-
-
-router.post('/', (req, res) => {
-    User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
 module.exports = router;
