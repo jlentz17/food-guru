3d7cd868333ced7c665d406c209c9356dbf32e76
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-08-04 20:00:10 -0700

    added seeds file

---
+++ seeds/index.js
@@ -0,0 +1,30 @@
+const seedUser = require('./user-seeds');
+const seedCategory = require('./cat-seeds');
+const seedRecipes = require('./recipe-seeds');
+const seedVotes = require('./vote-seeds');
+
+
+const sequelize = require('../config/connection');
+
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('--------------');
+
+    await seedUser();
+    console.log('--------------');
+
+    await seedCategory();
+    console.log('--------------');
+
+    await seedRecipes();
+    console.log('--------------');
+
+    await seedVotes();
+    console.log('--------------');
+
+
+    process.exit(0)
+};
+
+seedAll();
