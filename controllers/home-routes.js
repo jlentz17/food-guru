170a8629dae17976a1838f5ce2958b49017d8270
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-08-04 20:43:35 -0700

    deleted category file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,4 +2,5 @@ const router = require("express").Router();
 const sequelize = require("../config/connection")
 const { Recipe, User, Category, Vote } = require("../models")
 
+
 module.exports = router;
