bae47ed6ef84604e846dfd4bce78ba93d066131e
JazmyneB <bradleyj1@email.arizona.edu>
2021-08-04 20:43:08 -0700

    edited files in public/javascript to start working on views

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -1,27 +1,50 @@
 const router = require("express").Router();
 const { Category } = require("../../models");
-const withAuth = require("../../utils/auth")
+const withAuth = require("../../utils/auth");
   
-// router.get("/", (req, res) => {
-//     Category.findAll()
-//       .then((dbUserData) => res.json(dbUserData))
-//       .catch((err) => {
-//         console.log(err);
-//         res.statusCode(500).json(err);
-//       });
-//   });
+router.get("/", (req, res) => {
+    Category.findAll()
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.statusCode(500).json(err);
+      });
+  });
 
-//   router.post('/', (req, res) => {
-//     Category.create({
-//       category_name: req.body.category_name,
-//       recipe_id: req.body.recipe_id
-//     })
-//       .then(dbUserData => res.json(dbUserData))
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-//   });
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.body
+    }
+  })
+  .then(dbCatData => {
+    if (!dbCatData){
+      res.status(404).json({ message: 'No Category found with this ID'});
+      return;
+    }
+    res.json(dbCatData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+
+
+  })
+});
+
+router.post('/', (req, res) => {
+    Category.create({
+      category_name: req.body.category_name,
+      recipe_id: req.body.recipe_id
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
   
+
+
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -23,6 +23,6 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
 
 module.exports = router;
