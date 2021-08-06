5848c5ca0b44c5041baea249e617e90491feb8e7
JazmyneB <bradleyj1@email.arizona.edu>
2021-08-05 20:42:21 -0700

    updated login routes, able to login after signup

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,6 +11,38 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Recipe,
+        attributes: ['id', 'title', 'ingredients', 'recipe_content', 'created_at']
+      },
+      {
+        model: Recipe,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_recipe'
+      }
+    ]
+  })
+  .then((dbUserData) => {
+    if (!dbUserData){
+      res.status(404).json({ message: 'No user Found with this id'});
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+})
+
 
 router.post('/', (req, res) => {
     User.create({
@@ -23,6 +55,46 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
