172e85ad3842e64c5639b117d878ba0e63a85e7b
JazmyneB <bradleyj1@email.arizona.edu>
2021-08-09 16:03:41 -0700

    added comment model and routes

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,18 +4,4 @@ module.exports = {
       date
     ).getDate()}/${new Date(date).getFullYear()}`;
   },
-  format_url: (url) => {
-    return url
-      .replace("http://", "")
-      .replace("https://", "")
-      .replace("www.", "")
-      .split("/")[0]
-      .split("?")[0];
-  },
-  format_plural: (word, amount) => {
-    if (amount !== 1) {
-      return `${word}s`;
-    }
-    return word;
-  },
 };
